365d4c9d44af554752b2c74fbf682cb9f9bd319a
Tiago de Conto <tiagodc_89@yahoo.com.br>
2018-06-12 01:46:55 -0300

    first

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, send_from_directory #import main Flask class and request object
+import json, main, os
+
+app = Flask(__name__) #create the Flask app
+
+@app.route('/check', methods=['POST', 'GET'])
+def check():
+    testName = '__pcap__'
+    testFile = main.getPcap(testName)    
+    if(testFile != False):
+        cmd = 'rm --interactive=never pcaps/' + testName + '.pcap'
+        cmd = main.cmdMaker.makeCmd(cmd)
+        os.system(cmd)
+        testFile = True
+    return json.dumps(testFile)
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    # name = request.get_json()
+    name = 'teste'
+    hasFile = main.getPcap(name)
+    return json.dumps(hasFile)
+
+@app.route('/dir', methods=['GET'])
+def dir():
+    files = main.listPcaps()
+    return json.dumps( files )
+
+@app.route('/monitor', methods=['POST', 'GET'])
+def monitor():
+    # name = request.get_json()
+    name = 'teste'
+    fullName = 'pcap/' + name + '.pcap'
+    size = main.checkPcap(fullName)
+    return json.dumps( size )
+
+@app.route('/kill', methods=['GET'])
+def kill():
+    path = main.stopPcap()
+    return json.dumps(path)
+
+@app.route('/download/<path:filename>', methods=['GET','POST'])
+def download(filename):
+    path = os.getcwd() + '/pcaps'
+    return send_from_directory(path, filename)
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0', debug=True)
---
+++ main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+import os, sys, time
+
+class bashInfo:
+    sudoPass = 'tdc12237514'
+    workDir = '.'
+    baseCmd = 'echo {} | sudo -S '
+
+    def setPass(self, psw):
+        self.sudoPass = psw
+
+    def setwd(self, wd):
+        self.workDir = wd
+        os.chdir(wd)
+
+    def setBase(self):
+        return self.baseCmd.format(self.sudoPass)
+
+    def makeCmd(self, cmd):
+        return self.setBase() + cmd
+
+    def __init__(self):
+        self.setBase()
+
+cmdMaker = bashInfo()
+# cmdMaker.setwd('/home/tiago/Desktop/TLServer/')
+
+def checkPcap(fullFileName, erase=False):
+    sz1 = os.path.getsize( fullFileName )
+    
+    time.sleep(3)
+    sz2 = os.path.getsize( fullFileName )
+
+    if(sz2 == sz1):
+        cmd = 'kill -9 $(pidof tcpdump)'
+        if(erase): cmd +=  ' && rm --interactive=never ' + fullFileName
+        cmd = cmdMaker.makeCmd(cmd)
+        os.system( cmd )
+        return False
+    else:
+        return sz2
+
+def getPcap(fileName):
+    cmd = 'tcpdump src 192.168.1.201 and port 2368 or port 8308 -w pcaps/' + fileName + '.pcap &'
+    cmd = cmdMaker.makeCmd(cmd)
+    os.system(cmd)
+
+    time.sleep(3)
+    return checkPcap('pcaps/' + fileName + '.pcap', True)
+
+def stopPcap():
+    cmd = 'kill -9 $(pidof tcpdump)'
+    cmd = cmdMaker.makeCmd(cmd)
+    os.system( cmd )
+    return os.getcwd()
+
+def listPcaps():
+    return os.listdir('pcaps')
